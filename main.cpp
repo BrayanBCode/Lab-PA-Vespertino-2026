08265654b8d1df8deb0519af5a9f757f23a4354c
Codú <134159765+BrayanBCode@users.noreply.github.com>
2026-04-16 16:36:33 -0300

    refactor: si

--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@
 using namespace std;
 
 int main() {
-    Articulo * art1 = new Articulo(
+    auto * art1 = new Articulo(
         "10.1234/abc123",
         "Fundamentos de POO",
         new DTFecha(15, 5, 2023),
         "Programación Avanzada",
         "Introduccion a los principios fundamentales de la programacion orientada a objetos, explicando sus conceptos clave como clases, objetos, herencia y polimorfismo.");
 
-    Articulo * art2 = new Articulo(
+    auto * art2 = new Articulo(
         "10.4567/jkl012",
         "Utilidad de diagramas UML",
         new DTFecha(10, 2, 2024),
@@ -27,34 +27,34 @@ int main() {
     keyWords.push_back("Diseno");
     keyWords.push_back("OOP");
     keyWords.push_back("Class");
-    Libro * lib1 = new Libro(
+    auto * lib1 = new Libro(
         "10.2345/def456",
         "Patrones de Diseno en c++",
         new DTFecha(20, 8, 2022),
         "Software Design",
         keyWords);
 
-    // Reutilizo la lista ya que el constructor genera una copia del objeto list
+    // Reutilizo la lista, ya que el constructor genera una copia del objeto list
     keyWords.clear();
 
     keyWords.push_back("Diagramas");
     keyWords.push_back("UML");
     keyWords.push_back("Software");
     keyWords.push_back("Modelado");
-    Libro * lib2 = new Libro(
+    auto * lib2 = new Libro(
         "10.5678/mno345",
         "Guia de UML",
         new DTFecha(20, 8, 2022),
         "IEEE",
         keyWords);
 
-    Pagina_Web * pag1 = new Pagina_Web(
+    auto * pag1 = new Pagina_Web(
         "10.3456/ghi789",
         "Diagramas para Principiante",
         new DTFecha(20, 10, 2024), "www.umlparaprincipiantes.com",
         "En esta pagina web se presenta una gui completa sobre los diagramas UML, abordando los diagramas de casos de uso, de classes, de secuencia y deactividades.");
 
-    // Se debe utilizar "*" ya que estamos trabajando con punteros,
+    // Se debe utilizar "*", ya que estamos trabajando con punteros,
     // al utilizarlo hacemos referencia al objeto y no al puntero
     cout << *art1->getDT()<<endl;
     cout << *art2->getDT()<<endl;
@@ -63,8 +63,8 @@ int main() {
     cout << *pag1->getDT()<<endl;
     cout<<endl;
 
-    Investigador * inv1 = new Investigador("0000-0003-1234-5678", "Carla Oliveri", "Universidad de la Republica");
-    Investigador * inv2 = new Investigador("0000-0001-8765-4321", "Alberto Santos", "Instituto Tecnico");
+    auto * inv1 = new Investigador("0000-0003-1234-5678", "Carla Oliveri", "Universidad de la Republica");
+    auto * inv2 = new Investigador("0000-0001-8765-4321", "Alberto Santos", "Instituto Tecnico");
 
     cout<<inv1->toString()<<endl;
     cout<<inv2->toString()<<endl;
